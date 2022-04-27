15e1d7b3f0ed73cb519b2be813e231d5b99de98e
trankhoa2000 <khoa4755@gmail.com>
2022-04-27 20:59:56 +0700

    KhoaTM up code 27-04

--- a0321i1-cinema/src/app/module/home-page/home-page.module.ts
+++ a0321i1-cinema/src/app/module/home-page/home-page.module.ts
@@ -3,23 +3,25 @@ import { CommonModule } from '@angular/common';
 
 import { HomePageRoutingModule } from './home-page-routing.module';
 import { DetailMovieComponent } from './detail-movie/detail-movie.component';
-import { ContentComponent } from './content/content.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { ContentPageComponent } from './content-page/content-page.component';
 import { SearchComponent } from './search/search.component';
 import { TrailerComponent } from './trailer/trailer.component';
+import { ContentComponent } from './content/content.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
-  declarations: [DetailMovieComponent, ContentComponent, FooterComponent, HeaderComponent, ContentPageComponent, SearchComponent, TrailerComponent],
+  declarations: [DetailMovieComponent, FooterComponent, HeaderComponent, ContentPageComponent, SearchComponent, TrailerComponent, ContentComponent],
   exports: [
     HeaderComponent,
     FooterComponent
   ],
   imports: [
     CommonModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class HomePageModule { }
