560aa496983a527913925441f0669e7d606e5b13
duongdaiphuoc0501 <phuocrider25@gmail.com>
2022-05-13 18:50:30 +0700

    PhuocDD push code 13/5

--- a0321i1-cinema/src/app/app.component.ts
+++ a0321i1-cinema/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, ParamMap} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'a0321i1-cinema';
+
+  constructor(
+    public activeRoute: ActivatedRoute
+  ) {
+  }
+
+
+  ngOnInit(): void {
+    this.activeRoute.queryParams.subscribe((param) => {
+      console.log(param.paymentId)
+    })
+  }
 }
--- a0321i1-cinema/src/app/service/booking/book-tickets.service.ts
+++ a0321i1-cinema/src/app/service/booking/book-tickets.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {PaypalDTO} from '../../dto/PaypalDTO';
+import {tick} from "@angular/core/testing";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookTicketsService {
+  private API_INF = 'http://localhost:8080/api/movie_ticket';
+  private API_PAY = 'http://localhost:8080/api/payment';
 
-  constructor() { }
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    }),
+    'Access-Control-Allow-Origin': 'http://localhost:4200',
+    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+  };
+
+  constructor(
+    // tslint:disable-next-line:variable-name
+    private _httpClient: HttpClient
+  ) { }
+
+  getDetailMovie(id: string): Observable<any> {
+    return this._httpClient.get(this.API_INF + '/' + 'information' + '/' + id, this.httpOptions);
+  }
+
+  payViaPaypal(price: any): Observable<any>{
+    return this._httpClient.post<PaypalDTO>(this.API_PAY + '/' + 'pay', {"price" : price}, this.httpOptions);
+  }
+
+  sendMailSuccessful(ticketInfo: any){
+    return this._httpClient.post(this.API_PAY + '/successful', ticketInfo,this.httpOptions);
+  }
 }
